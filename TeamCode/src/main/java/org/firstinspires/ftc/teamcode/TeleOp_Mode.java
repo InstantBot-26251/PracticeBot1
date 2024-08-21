2cd6eb521a66766af6949fb3b58facde4aad30cc
Lakshya <lakshyakhandelwal28@gmail.com>
2024-08-21 11:35:21 -0700

    Made Mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Mode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Mode.java
@@ -8,35 +8,66 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp (name = "lakshyaCode")
 public class TeleOp_Mode extends OpMode {
+    // Making variables for each motor
     DcMotorEx fl;
     DcMotorEx fr;
     DcMotorEx bl;
     DcMotorEx br;
+
+    // Making the x & y variables
+    double y;
+    double x;
+    double rx;
+
+    // Making the mechanum drive
+    double denominator;
+    double frontLeftPower;
+    double backLeftPower;
+    double frontRightPower;
+    double backRightPower;
+
     @Override
     public void init() {
+        //Setting motors hardware
         fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
         fr = hardwareMap.get(DcMotorEx.class, "frontRight");
         bl = hardwareMap.get(DcMotorEx.class, "backLeft");
         br = hardwareMap.get(DcMotorEx.class, "backRight");
 
+        //Brake
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        //Reverse
         br.setDirection(DcMotorSimple.Direction.REVERSE);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        fl.setPower(gamepad1.left_stick_y);
-        fr.setPower(gamepad1.right_stick_y);
-        bl.setPower(gamepad1.left_stick_y);
-        br.setPower(gamepad1.right_stick_y);
+        y = -gamepad1.left_stick_y;
+        x = gamepad1.left_stick_x * 1.1;
+        rx = gamepad1.right_stick_x;
 
-        telemetry.addData("left stick y", gamepad1.left_stick_y);
-        telemetry.addData("right stick y", gamepad1.right_stick_y);
+        denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        frontLeftPower = (y + x + rx) / denominator;
+        backLeftPower = (y - x + rx) / denominator;
+        frontRightPower = (y - x - rx) / denominator;
+        backRightPower = (y + x - rx)/ denominator;
+
+        fl.setPower(frontLeftPower);
+        fr.setPower(backLeftPower) ;
+        bl.setPower(frontRightPower);
+        br.setPower(backRightPower);
+
+
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("Left stick x", gamepad1.left_stick_x);
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        
         telemetry.update();
     }
 }
