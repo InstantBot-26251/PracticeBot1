47460a01c207b4d6451b27d05dfe1b6441565c72
Ishaan <ishaan.debch12@gmail.com>
2024-08-21 11:34:53 -0700

    I made mecanum drive!!! :)

--- TeamCode/src/main/java/TeleOpMode.java
+++ TeamCode/src/main/java/TeleOpMode.java
@@ -2,6 +2,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 
@@ -33,8 +34,10 @@ public class TeleOpMode extends OpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
-
+//LIBRARIES?!?!?!?!?!?!?!?!?! for servos
     @Override
     public void loop() {
 
@@ -44,12 +47,12 @@ public class TeleOpMode extends OpMode {
         telemetry.addData("RS-X", gamepad1.right_stick_x);
 
         y = gamepad1.left_stick_y;
-        x = gamepad1.left_stick_x;
-        rx = gamepad1.right_stick_x;
+        x = -gamepad1.left_stick_x;
+        rx = -gamepad1.right_stick_x;
         denominator = Math.max(Math.abs(x) + Math.abs(y) + Math.abs(rx), 1);
         frontLeftPower = (y + x + rx) / denominator;
-        frontRightPower = (y - x - rx) / denominator;
         backLeftPower = (y - x + rx) / denominator;
+        frontRightPower = (y - x - rx) / denominator;
         backRightPower = (y + x - rx) / denominator;
 
         frontRight.setPower(frontRightPower);
@@ -58,3 +61,10 @@ public class TeleOpMode extends OpMode {
         backRight.setPower(backRightPower);
     }
 }
+//Laptop and TV and Game manual, Java, Inventr teach and learn, 3d pronter wire from cieling
+// STMS Scedule, Teen Adv Brd., New RAV4, etc. etc., etc. NEW ROUTER
+//Setup new laptop
+//Redo wheels.
+//New shoes
+//Roblox
+
