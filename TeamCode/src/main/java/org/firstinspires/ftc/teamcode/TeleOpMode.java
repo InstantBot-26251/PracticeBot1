781dd7fc86653d3e235988cad6a65dc112802efd
Vyan <vyma.2030@gmail.com>
2024-08-21 10:46:02 -0800

    Added Mecanum Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -13,28 +13,57 @@ public class TeleOpMode extends OpMode {
     DcMotorEx bl;
     DcMotorEx br;
 
+    double y;
+    double x;
+    double rx;
+    double denominator;
+    double frontLeftPower;
+    double backLeftPower;
+    double frontRightPower;
+    double backRightPower;
+
+
     @Override
     public void init() {
         fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
-        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr = hardwareMap.get(DcMotorEx.class, "frontRight");
-        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl = hardwareMap.get(DcMotorEx.class, "backLeft");
-        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
+
     }
 
     @Override
     public void loop() {
-        fl.setPower(gamepad1.left_stick_y);
-        fr.setPower(gamepad1.right_stick_y);
-        bl.setPower(gamepad1.left_stick_y);
-        br.setPower(gamepad1.right_stick_y);
+        y = gamepad1.left_stick_y;
+        x = -gamepad1.left_stick_x * 1.1;
+        rx = -gamepad1.right_stick_x;
+        denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        frontLeftPower = (y + x + rx) / denominator;
+        backLeftPower = (y - x + rx) / denominator;
+        frontRightPower = (y - x - rx) / denominator;
+        backRightPower = (y + x - rx) / denominator;
+
+        fl.setPower(frontLeftPower);
+        fr.setPower(frontRightPower);
+        bl.setPower(backLeftPower);
+        br.setPower(backRightPower);
+
         telemetry.addData("left stick y" , gamepad1.left_stick_y);
         telemetry.addData("right stick y" , gamepad1.right_stick_y);
+        telemetry.addData("left stick x" , gamepad1.left_stick_x);
+        telemetry.addData("right stick x" , gamepad1.right_stick_x);
+        telemetry.addData("Vyan:", "The Great");
+
         telemetry.update();
+
+
     }
 }
