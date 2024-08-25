a0e4f68b327703e72a32a843c25bfa00c0b7c671
Lakshya <lakshyakhandelwal28@gmail.com>
2024-08-25 15:19:43 -0700

    Made Field Centric & Response Curve

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisLakshya.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisLakshya.java
@@ -1,51 +1,67 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class ChassisLakshya {
-    DcMotorEx fl;
-    DcMotorEx fr;
-    DcMotorEx bl;
-    DcMotorEx br;
+    DcMotor fl, fr, bl, br;
+    IMU imu;
 
-    double denominator;
-    double frontLeftPower;
-    double frontRightPower;
-    double backLeftPower;
-    double backRightPower;
+    public ChassisLakshya(HardwareMap hardwareMap) {
+        // Initialize motors
+        fl = hardwareMap.get(DcMotor.class, "frontLeft");
+        fr = hardwareMap.get(DcMotor.class, "frontRight");
+        bl = hardwareMap.get(DcMotor.class, "backLeft");
+        br = hardwareMap.get(DcMotor.class, "backRight");
 
-    public ChassisLakshya (HardwareMap hardwareMap) {
-        //Setting motor hardware
-        fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
-        fr = hardwareMap.get(DcMotorEx.class, "frontRight");
-        bl = hardwareMap.get(DcMotorEx.class, "backLeft");
-        br = hardwareMap.get(DcMotorEx.class, "backRight");
-
-        //Brake
+        // Set motor directions and zero power behavior
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Reverse
-        br.setDirection(DcMotorSimple.Direction.REVERSE);
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Initialize x
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        // Set up IMU parameters
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        ));
+        imu.initialize(parameters);
+    }
+    public void resetYaw() {
+        imu.resetYaw();
     }
 
-    public void drive (double x, double y, double rx) {
-        denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        frontLeftPower = (y + x + rx) / denominator;
-        backLeftPower = (y - x + rx) / denominator;
-        frontRightPower = (y - x - rx) / denominator;
-        backRightPower = (y + x - rx)/ denominator;
+    public void drive(double x, double y, double rx) {
+        // Get the robot's current heading
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
+        // Adjust the input values for field-centric control
+        double adjustedX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double adjustedY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        adjustedX = adjustedX * 1.1;  // Counteract imperfect strafing
+
+        double denominator = Math.max(Math.abs(adjustedY) + Math.abs(adjustedX) + Math.abs(rx), 1);
+        double frontLeftPower = (adjustedY + adjustedX + rx) / denominator;
+        double backLeftPower = (adjustedY - adjustedX + rx) / denominator;
+        double frontRightPower = (adjustedY - adjustedX - rx) / denominator;
+        double backRightPower = (adjustedY + adjustedX - rx) / denominator;
+
+        // Set motor powers
         fl.setPower(frontLeftPower);
         fr.setPower(frontRightPower);
         bl.setPower(backLeftPower);
         br.setPower(backRightPower);
-
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Mode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Mode.java
@@ -3,11 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp (name = "lakshyaCode")
+@TeleOp(name = "lakshyaCode")
 public class TeleOp_Mode extends OpMode {
+    LiftLakshya theBetterLift;
     ChassisLakshya theBetterChassis;
     double x;
     double y;
@@ -16,21 +15,38 @@ public class TeleOp_Mode extends OpMode {
     @Override
     public void init() {
         theBetterChassis = new ChassisLakshya(hardwareMap);
+        theBetterLift = new LiftLakshya(hardwareMap);
+
+        theBetterLift.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     @Override
     public void loop() {
-        y = -gamepad1.left_stick_y;
-        x = gamepad1.left_stick_x * 1.1;
-        rx = gamepad1.right_stick_x;
+        y = applyResponseCurve(gamepad1.left_stick_y);
+        x = -applyResponseCurve(gamepad1.left_stick_x);
+        rx = -applyResponseCurve(gamepad1.right_stick_x);
 
+        telemetry.addData("Lift Encoder", theBetterLift.encoderValue());
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
         telemetry.addData("Right stick x", gamepad1.right_stick_x);
-        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        telemetry.addData("Adjusted x", x);
+        telemetry.addData("Adjusted y", y);
+        telemetry.addData("Adjusted rx", rx);
+
+        if(gamepad1.y){
+            theBetterChassis.resetYaw();
+        }
 
         theBetterChassis.drive(x, y, rx);
+        theBetterLift.drive(-gamepad2.left_stick_y);
 
         telemetry.update();
     }
+
+    public double applyResponseCurve(double input) {
+        double exponent = 2;
+        return Math.signum(input) * Math.pow(Math.abs(input), exponent);
+    }
+
 }
