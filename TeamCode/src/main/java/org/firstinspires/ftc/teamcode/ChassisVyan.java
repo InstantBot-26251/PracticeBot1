a6829b9cbac74b01d60feb1fbb0268a1f215ea85
CoderPersonOnGit <vyma.2030@gmail.com>
2024-08-25 14:28:14 -0800

    3 vyanchassis (#12)
    * Add Tank Drive.
    
    * Added initialization telemetry
    
    * Added Mecanum Drive
    
    * Added Mecanum Drive
    
    * Added liftkit, Response Curves and Field Centric.
    
    * Refactored

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisVyan.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class ChassisVyan {
+
+    DcMotorEx fl;
+    DcMotorEx fr;
+    DcMotorEx bl;
+    DcMotorEx br;
+    IMU imu;
+    IMU.Parameters parameters;
+    double rotX;
+    double rotY;
+    double botHeading;
+
+    public ChassisVyan(HardwareMap hardwareMap) {
+        fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        fr = hardwareMap.get(DcMotorEx.class, "frontRight");
+        bl = hardwareMap.get(DcMotorEx.class, "backLeft");
+        br = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+
+                imu.initialize(parameters);
+
+    }
+
+    public void drive(double x, double y, double rx) {
+
+        botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+
+        rotX = rotX * 1.1;
+
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        fl.setPower(frontLeftPower);
+        fr.setPower(frontRightPower);
+        bl.setPower(backLeftPower);
+        br.setPower(backRightPower);
+
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VyanTeleOpMode.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "VyanTeleOp")
+public class VyanTeleOpMode extends OpMode {
+    ChassisVyan driveTrain;
+    LiftVyan liftKit;
+
+    double y;
+    double x;
+    double rx;
+
+    @Override
+    public void init() {
+        driveTrain = new ChassisVyan(hardwareMap);
+        liftKit = new LiftVyan(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        y = applyResponseCurve(gamepad1.left_stick_y);
+        x = -applyResponseCurve(gamepad1.left_stick_x);
+        rx = -applyResponseCurve(gamepad1.right_stick_x);
+
+        driveTrain.drive(x, y, rx);
+        liftKit.drive(-gamepad2.left_stick_y);
+
+        telemetry.addData("Initialization", "Complete");
+        telemetry.addData("left stick y", gamepad1.left_stick_y);
+        telemetry.addData("right stick y", gamepad1.right_stick_y);
+        telemetry.addData("left stick x", gamepad1.left_stick_x);
+        telemetry.addData("right stick x", gamepad1.right_stick_x);
+        telemetry.addData("Right gamepad Left stick y", gamepad2.left_stick_y);
+        telemetry.addData("LiftEncoderValue", liftKit.getEncoderValue());
+        telemetry.addData("Vyan", "THE Great");
+
+        telemetry.update();
+
+
+    }
+
+    public double applyResponseCurve(double input) {
+        double exponent = 2;
+        return Math.signum(input) * Math.pow(Math.abs(input), exponent);
+    }
+}
